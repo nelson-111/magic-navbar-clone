c90c7546002385cc400e4f7828280766dc89cb8d
Jackdanelrub <jackdanelrubhasher@gmail.com>
2022-10-02 10:57:04 -0500

    github page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,29 +5,37 @@
   import {routes} from '../routes'
 </script>
 
-<div style={`--color-1: hsl(${Math.floor(Math.random() * 360)} 100% 5%); --color-2: hsl(${Math.floor(Math.random() * 360)} 80% 92%)`} class="bg-[color:var(--color-1)] flex justify-center items-center min-h-[100vh]">
+<div style={`--color-1: hsl(${Math.floor(Math.random() * 360)} 100% 5%); --color-2: hsl(${Math.floor(Math.random() * 360)} 80% 92%)`}
+class="bg-[color:var(--color-1)] text-[color:var(--color-2)] p-[10vmax]">
+  <a href="https://github.com/nelson-111/Magic-navbar-clone"
+  class="flex justify-evenly text-[4vmax]">
+    Github page
+  </a>
 
-<!--The navbar-->
-  <div class="w-[50vmax] h-[8vmax] bg-[color:var(--color-2)] flex justify-evenly items-center rounded-[1vmax] relative" title="Navbar">
+  <div class="flex justify-center items-center min-h-[50vh]">
 
-    <!--It renders every icon-->
-    {#each routes as route}
-    <a href="/" class="z-10 text-center group flex justify-center items-center p-[1vmax]" title={route.name}>
+  <!--The navbar-->
+    <div class="w-[50vmax] h-[8vmax] bg-[color:var(--color-2)] flex justify-evenly items-center rounded-[1vmax] relative" title="Navbar">
 
-      <!--The icon container-->
-      <div class="mx-auto bg-[color:var(--color-2)] rounded-[100%] p-[1.1vmax] h-[5vmax] w-[5vmax] group-hover_bg-[color:var(--color-1)] [transition:500ms] group-hover_translate-y-[-4.2vmax] absolute group-active_bg-[color:var(--color-1)] group-active_translate-y-[-4.2vmax]">
+      <!--It renders every icon-->
+      {#each routes as route}
+      <a href="/" class="z-10 text-center group flex justify-center items-center p-[1vmax]" title={route.name}>
 
-        <!--The icon-->
-        <div class="fill-[color:var(--color-1)] drop-shadow-[0_0_0.15vmax_var(--color-2)] group-hover_fill-[color:var(--color-2)] group-active_fill-[color:var(--color-2)] [transition:500ms]">
-          {@html route.icon}
+        <!--The icon container-->
+        <div class="mx-auto bg-[color:var(--color-2)] rounded-[100%] p-[1.1vmax] h-[5vmax] w-[5vmax] group-hover_bg-[color:var(--color-1)] [transition:500ms] group-hover_translate-y-[-4.2vmax] absolute group-active_bg-[color:var(--color-1)] group-active_translate-y-[-4.2vmax]">
+
+          <!--The icon-->
+          <div class="fill-[color:var(--color-1)] drop-shadow-[0_0_0.15vmax_var(--color-2)] group-hover_fill-[color:var(--color-2)] group-active_fill-[color:var(--color-2)] [transition:500ms]">
+            {@html route.icon}
+          </div>
         </div>
-      </div>
 
-      <!--The text-->
-      <div class="text-[1.5vmax] text-[color:var(--color-1)] lg_font-['Chakra_Petch'] font-['Arial'] [text-shadow:0_0_0.4vmax_var(--color-1)] opacity-0 group-hover_opacity-100 group-active_opacity-100 [transition:500ms]">
-        {route.name}
-      </div>
-    </a>
-    {/each}
+        <!--The text-->
+        <div class="text-[1.5vmax] opacity-0 group-hover_opacity-100 group-active_opacity-100 [transition:500ms] text-[color:var(--color-1)] [text-shadow:0_0_0.4vmax_var(--color-1)]">
+          {route.name}
+        </div>
+      </a>
+      {/each}
+    </div>
   </div>
 </div>
