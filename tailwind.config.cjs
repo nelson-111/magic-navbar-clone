0356efc94a4c5fed426b4d6f636e98ec24cd70cf
null <85085896+null010101@users.noreply.github.com>
2023-05-21 10:58:58 -0500

    Delete tailwind.config.cjs

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  separator: '_',
-  content: ['./src/**/*.{html,js,svelte,ts,jsx,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
